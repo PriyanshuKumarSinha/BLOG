2dc7523ac52fcfc396af867ed9975377b70e7a18
Priyanshu Kumar Sinha <79042642+priyanshukumarsinha@users.noreply.github.com>
2024-04-28 15:46:49 +0000

    Checking Components

--- blog/pages/Home.jsx
+++ blog/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import Container from '../src/components/container/Container'
 import Button from '../src/components/Button'
+import Input from '../src/components/Input'
+import Select from '../src/components/Select'
+import PostCard from '../src/components/PostCard'
+import Footer from '../src/components/Footer'
 
 function Home() {
+  const options = ["lorem", "ipsum", "hello"]
   return (
       <Container>
         <h1>Home</h1>
@@ -10,6 +15,16 @@ function Home() {
         <Button>
           Button Done
         </Button>
+
+        <Input label= "username">
+        </Input>
+
+        <Select options = {options}
+          label = "Status"
+        >
+        </Select>
+        <PostCard></PostCard>
+        <Footer/>
       </Container>
   )
 }
