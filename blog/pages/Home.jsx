4de084051fdca988fd9df22eea7c0efcd54e4731
Priyanshu Kumar Sinha <79042642+priyanshukumarsinha@users.noreply.github.com>
2024-04-28 20:43:47 +0000

    Tested all the Components, We need to fix something called a CORS error

--- blog/pages/Home.jsx
+++ blog/pages/Home.jsx
@@ -5,17 +5,22 @@ import Input from '../src/components/Input'
 import Select from '../src/components/Select'
 import PostCard from '../src/components/PostCard'
 import Footer from '../src/components/Footer/Footer'
+import Header from '../src/components/Header/Header'
+import LogoutBtn from '../src/components/Header/LogoutBtn'
+import RTE from '../src/components/RTE'
+import PostForm from '../src/components/Post-orm/PostForm'
 
 function Home() {
   const options = ["lorem", "ipsum", "hello"]
   return (
-      <Container>
+    <Container>
+        <PostForm/>
         <h1>Home</h1>
 
         <Button>
           Button Done
         </Button>
-
+        <LogoutBtn />
         <Input label= "username">
         </Input>
 
@@ -24,7 +29,14 @@ function Home() {
         >
         </Select>
         <PostCard></PostCard>
+
+        <Header>
+          
+        </Header>
+
         <Footer/>
+        
+
       </Container>
   )
 }
