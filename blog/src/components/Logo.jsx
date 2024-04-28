43935b514f7c2dcbfc340f2240dd96665e942f80
Priyanshu Kumar Sinha <79042642+priyanshukumarsinha@users.noreply.github.com>
2024-04-28 14:45:41 +0000

    Logo Done : have to add image

---
+++ blog/src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Logo(width = "100%") {
+  return (
+    // <div>Logo</div>
+    <img src="" style= {width} alt="Logo Place Holder" />
+  )
+}
+
+export default Logo
